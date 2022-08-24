881df8acdeab6afe47323ac61c664f9c27efb74b
milonte <milonte@gmail.com>
2022-08-24 14:54:13 +0200

    feat: photographer card

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,14 +4,23 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
+
         const article = document.createElement('article');
-        const img = document.createElement('img');
-        img.setAttribute("src", picture)
-        const h2 = document.createElement('h2');
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        article.innerHTML = `<a href="photographer.html">
+                                <img src="${picture}" alt="${name}" />
+                                <h2>${name}</h2>
+                            </a>
+                            <aside>
+                                <p class="city">${city}, ${country}</p>
+                                <p class="tagline">${tagline}</p>
+                                <p class="price">${price}€/jour</p>
+                            </aside>`;
+
+
         return (article);
     }
+
     return { name, picture, getUserCardDOM }
 }
